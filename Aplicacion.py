63b43c0f8f8a171b5be025a214f6f0fbc3fe36bd
Sebastian Rakauskas <sebastianalberto258@hotmail.com>
2022-11-09 09:16:25 -0500

    Parte 2 finalizada por Sebastian Rakauskas

--- Aplicacion.py
+++ Aplicacion.py
@@ -1,3 +1,19 @@
+import sqlite3
+
+conexion = sqlite3.connect("Rakauskas_Quispe_Prado_almacen.db")
+tabla_producto =""" CREATE TABLE Producto(
+            idproducto INTEGER PRIMARY KEY AUTOINCREMENT,
+            codigo TEXT UNIQUE,
+            nombre TEXT,
+            precio FLOAT
+            )
+       """
+
+cursor = conexion.cursor()
+cursor.execute(tabla_producto)
+
+conexion.close()
+
 print("\tMenu de opciones\n")
 print("1. Registrar\n")
 print("2. Eliminar\n")
