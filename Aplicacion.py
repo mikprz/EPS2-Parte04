97e5fb21d9e77f18c06997b2f8cce0566dd20312
Sebastian Rakauskas <sebastianalberto258@hotmail.com>
2022-11-09 08:53:25 -0500

    Parte 1 finalizada por Sebastian Rakauskas

---
+++ Aplicacion.py
@@ -0,0 +1,8 @@
+print("\tMenu de opciones\n")
+print("1. Registrar\n")
+print("2. Eliminar\n")
+print("3. Editar\n")
+print("4. Listar\n")
+print("5. Salir\n")
+
+opcion=input("\nSeleccione una opcion: ")
